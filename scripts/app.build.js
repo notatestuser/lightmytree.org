4849f2ece8d41ff3c6e1ba79123aebd25286a5f3
Luke Plaster <notatestuser@gmail.com>
2012-11-21 13:48:10 +0000

    require.js optimiser build script

---
+++ scripts/app.build.js
@@ -0,0 +1,19 @@
+({
+	appDir: "../assets",
+
+	baseUrl: "js",
+
+	dir: "../assets_live",
+
+	mainConfigFile: "../assets/js/config.js",
+
+	optimize: "uglify",
+	optimizeCss: "standard",
+	removeCombined: true,
+
+	modules: [
+		{
+			name: "main"
+		}
+	]
+})
